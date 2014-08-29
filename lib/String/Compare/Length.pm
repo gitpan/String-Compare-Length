d3685ec3bbc378c878dd2a1e2e514e52918ad956
Michael Accardo <accardo@cpan.org>
2014-08-29 20:49:56 +0000

    Import of ACCARDO/String-Compare-Length-0.04 from CPAN.
    gitpan-cpan-distribution: String-Compare-Length
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         ACCARDO/String-Compare-Length-0.04.tar.gz
    gitpan-cpan-author:       ACCARDO
    gitpan-cpan-maturity:     released

--- lib/String/Compare/Length.pm
+++ lib/String/Compare/Length.pm
@@ -5,7 +5,7 @@ use warnings;
 
 our @ISA = qw(Exporter);
 our @EXPORT = qw(compare_array compare_arrays compare_hoa);
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 sub array_avg_length {
   my $array = shift;
